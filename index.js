179e1911d01c21649b1d8a419365c76b22704984
juber131 <jubayerahmad131@gmail.com>
2024-12-09 23:09:44 +0600

    add new section

---
+++ index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const slides = document.querySelectorAll('.slide');
+    let currentIndex = 0;
+
+    setInterval(() => {
+      // Remove 'active' class from the current slide
+      slides[currentIndex].classList.remove('active');
+
+      // Move to the next slide
+      currentIndex = (currentIndex + 1) % slides.length;
+
+      // Add 'active' class to the next slide
+      slides[currentIndex].classList.add('active');
+    }, 7000); // Each slide stays for 7 seconds
+  });
+
+  
