64961b7bbbe77ac49cc170c40ccca7a932ba8b50
juber131 <jubayerahmad131@gmail.com>
2024-12-12 00:06:15 +0600

    add image section

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const slides = document.querySelectorAll('.slide');
+    const slides = document.querySelectorAll('.slide-1');
     let currentIndex = 0;
 
     setInterval(() => {
       // Remove 'active' class from the current slide
-      slides[currentIndex].classList.remove('active');
+      slides[currentIndex].classList.remove('active-1');
 
       // Move to the next slide
       currentIndex = (currentIndex + 1) % slides.length;
 
       // Add 'active' class to the next slide
-      slides[currentIndex].classList.add('active');
+      slides[currentIndex].classList.add('active-1');
     }, 7000); // Each slide stays for 7 seconds
   });
 
   
-  
+
